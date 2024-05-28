26978c92aa128252104c7f96392ec73d79285980
eoqna <eoqna@naver.com>
2024-05-28 23:18:41 +0900

    배경 이미지 및 초기화면 css 변경

---
+++ src/assets/constants/title.ts
@@ -0,0 +1,50 @@
+interface TextProps {
+  text: string;
+  color: string;
+};
+
+export const top: TextProps[] = [
+  {
+    text: "해",
+    color: "red",
+  },
+  {
+    text: "인",
+    color: "orange",
+  },
+  {
+    text: "의",
+    color: "yellow",
+  },
+  {
+    text: "생",
+    color: "green",
+  },
+  {
+    text: "일",
+    color: "blue",
+  },
+];
+
+export const bottom: TextProps[] = [
+  {
+    text: "축",
+    color: "blue",
+  },
+  {
+    text: "하",
+    color: "blue",
+  },
+  {
+    text: "합",
+    color: "blue",
+  },
+  {
+    text: "니",
+    color: "blue",
+  },
+  {
+    text: "다",
+    color: "blue",
+  },
+]
--- src/screen/Congratulation.tsx
+++ src/screen/Congratulation.tsx
@@ -5,6 +5,7 @@ import { Colors } from "../assets/css/colors";
 import image1 from "../assets/imgs/leaveMouse.png";
 import image2 from "../assets/imgs/enterMouse.jpg";
 import background from "../assets/imgs/background.jpg";
+import { bottom, top } from "../assets/constants/title";
 
 const Layout = styled.div`
   width: 100%;
@@ -25,10 +26,18 @@ const ContentLayout = styled.div`
   align-items: center;
 `;
 
-const Text = styled.h2`
-  font-size: 4vw;
+const TextLayout = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const Text = styled.span<{ $color: string }>`
+  font-size: 5vw;
   font-weight: bold;
-  color: ${Colors.White};
+  color: ${(props) => props.$color};
+  -webkit-text-stroke-width: 3px;
+  -webkit-text-stroke-color: ${Colors.Black};
 `;
 
 const ImgLayout = styled.div`
@@ -86,7 +95,11 @@ const Congratulation = (props: CommonProps.ComponentProps) => {
   return (
     <Layout>
       <ContentLayout className="title-layout">
-        <Text>Congratulation</Text>
+        <TextLayout>
+          {top.map((item) => (
+            <Text key={item.text} $color={item.color}>{item.text}</Text>
+          ))}
+        </TextLayout>
         <ImgLayout>
           <Img 
             src={image1} 
@@ -97,7 +110,11 @@ const Congratulation = (props: CommonProps.ComponentProps) => {
           />
           <ImgText className="img-text">Click Me !</ImgText>
         </ImgLayout>
-        <Text>Haein's Birthday</Text>
+        <TextLayout>
+          {bottom.map((item) => (
+            <Text key={item.text} $color={item.color}>{item.text}</Text>
+          ))}
+        </TextLayout>
       </ContentLayout>
     </Layout>
   );
