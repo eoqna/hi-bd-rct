b725507915eae3ac3a92719d7f4e762669d31966
eoqna <eoqna@naver.com>
2024-06-03 23:18:22 +0900

    SecondGame 스크린 추가

--- src/navigations/Navigations.tsx
+++ src/navigations/Navigations.tsx
@@ -7,6 +7,7 @@ import Start from "../screen/Start";
 import Second from "../screen/Second";
 import Third from "../screen/Third";
 import Endding from "../screen/Endding";
+import SecondGame from "../screen/SecondGame";
 
 const Layout = styled.div`
   width: 100%;
@@ -30,6 +31,7 @@ const Navigations = () => {
           <Route path="start" element={<Start navigation={navigation} />}/>
           <Route path="/nonograms" element={<Nonograms navigation={navigation} />} />
           <Route path="/second" element={<Second navigation={navigation} />} />
+          <Route path="/second-game" element={<SecondGame navigation={navigation} />} />
           <Route path="/third" element={<Third navigation={navigation} />} />
           <Route path="/endding" element={<Endding navigation={navigation} />} />
         </Routes>
